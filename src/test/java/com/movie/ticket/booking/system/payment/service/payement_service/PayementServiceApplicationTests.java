b2a325c95330edd862a26a4694bf132e880877bf
OM Prakash singh <opsingh576@gmail.com>
2024-09-12 15:49:43 +0530

    Initial commit

---
+++ src/test/java/com/movie/ticket/booking/system/payment/service/payement_service/PayementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.ticket.booking.system.payment.service.payement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
